a217d99efc85b16de41d92fa1f7e77de5cbd5ec2
Minsu Jung <nou0983@gmail.com>
2022-12-14 22:33:18 +1100

    completed menu project

--- react-projects/05-menu/setup/src/App.js
+++ react-projects/05-menu/setup/src/App.js
@@ -1,10 +1,59 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState, useEffect } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      return items;
+    }
+
+    const newItems = items.filter((item) => {
+      if (item.category === category) {
+        return item;
+      }
+    });
+
+    return newItems;
+  };
+
+  const filterHandler = (category) => {
+    setMenuItems(filterItems(category));
+  };
+
+  useEffect(() => {
+    // Using reduce method to filter out the unique category values
+    // const newCategories = menuItems.reduce(
+    //   (acc, item) => {
+    //     if (!acc.includes(item.category)) {
+    //       acc.push(item.category);
+    //     }
+
+    //     return acc;
+    //   },
+    //   ["all"]
+    // );
+
+    // Using set data to filter out the unique category values
+    const newCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+    setCategories(newCategories);
+  }, []);
+
+  return (
+    <main className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories} filterHandler={filterHandler} />
+      <Menu items={menuItems} />
+    </main>
+  );
 }
 
 export default App;
--- react-projects/05-menu/setup/src/Categories.js
+++ react-projects/05-menu/setup/src/Categories.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterHandler }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, i) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={i}
+            onClick={() => {
+              filterHandler(category);
+            }}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
