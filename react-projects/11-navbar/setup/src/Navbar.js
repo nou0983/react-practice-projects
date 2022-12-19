d448ab877a3d7287d69fcc5b6cfc2f42937ddb6b
Minsu Jung <nou0983@gmail.com>
2022-12-19 22:04:06 +1100

    completed navbar project

--- react-projects/11-navbar/setup/src/Navbar.js
+++ react-projects/11-navbar/setup/src/Navbar.js
@@ -1,10 +1,62 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [isShow, setIsShow] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-export default Navbar
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+    if (isShow) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0";
+    }
+  }, [isShow]);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <a href="/">
+            <img src={logo} alt="Coding addict" />
+          </a>
+          <button className="nav-toggle" onClick={() => setIsShow(!isShow)}>
+            <FaBars />
+          </button>
+        </div>
+
+        <div
+          className={`links-container ${isShow && "show-container"}`}
+          ref={linksContainerRef}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map(({ id, url, text }) => {
+              return (
+                <li key={id}>
+                  <a href={url}> {text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <ul className="social-icons">
+          {social.map(({ id, url, icon }) => {
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
