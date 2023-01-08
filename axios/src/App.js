7ae53c183c380e4020d36969a61721c04845b03a
Minsu Jung <nou0983@gmail.com>
2023-01-08 17:06:16 +1100

    completed axios

---
+++ axios/src/App.js
@@ -0,0 +1,14 @@
+import Title from "./components/Title";
+import Setup from "./examples/3-post-request";
+// import './axios/global';
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
